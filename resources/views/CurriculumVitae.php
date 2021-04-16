16705bb5173ccffefc64ecf865859b3e0fd770c8
RazwaSakha <razpradipa31@gmail.com>
2021-04-16 13:36:54 +0700

    First Commit

---
+++ resources/views/CurriculumVitae.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>CURRICULUM VITAE</title>
+    </head>
+    <body>
+    <h1 align=center>CURRICULUM VITAE</h1>
+    <!--INFORMASI DATA DIRI-->
+        <hr>
+        <b>Personal Information</b>
+        <hr>
+        <table style="width:50%">
+            <!--FOTO-->
+            <img src="IMG_1691.jpg" align="right" width="104" height="142">
+        <tr>
+        <th></th>
+        <th></th>
+        </tr>
+        <tr>
+            <td>Name</td>
+            <td>: Razwa Sakha Pradipa </td>
+        </tr>
+        <tr>
+            <td>Date of birth</td>
+            <td>: Sukabumi, 03 January 2004 </td>  
+        </tr>
+            <td>Address</td>
+            <td>: Perumahan Genting puri, Kec.Baros, Kota Sukabumi</td>
+        <tr>
+            <td>Status</td>
+            <td>: Student</td>
+        </tr>
+            <tr>
+            <td>Telp. Number</td>
+            <td>: 083805421781</td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td>: razpradipa31@gmail.com</td>
+        </tr>
+        </table>
+    <!--RIWAYAT PENDIDIKAN FORMAL-->  
+            <hr>
+            <b>Personal Education</b>
+            <hr>
+                <table style="width:80%">
+            <tr>
+            <th></th>
+            <th></th>
+            </tr>
+            <tr>
+                <td>2019-2023</td>
+                <td>SMKN 2 Sukabumi </td>
+            </tr>
+            <tr>
+                <td>2016-2019</td>
+                <td>SMPT Darul Amal </td>  
+            </tr>
+                <td>2010-2016</td>
+                <td>SDN Cipanas</td>
+            <tr>
+                <td>2008-2010</td>
+                <td>TK Pembina Lembursitu</td>
+            </tr>
+            </table>
+    <!-- RIWAYAT ORGANISASI-->
+                            <hr>
+                            <b>ORGANIZATION</b><hr>
+                            <table style="width:180%">
+                        <tr>
+                        <th></th>
+                        <th></th>
+                        </tr>
+                        <tr>
+                            <td>2020</td>
+                            <td>Paskibra</td>
+                        </tr>
+                        </table>
+    <!--KEAHLIAN YANG DIMILIKI-->                  
+                            <hr>
+                            <b>SKILLS</b><hr>
+                        Building Computer<br>
+                        Graphic Desain: CorelDraw<br>
+                        Video Editting: Adobe Premiere <br>
+    <a href="indeks.html">BACK TO INDEX</a><br>
+    </body>
+</html>
---
+++ resources/views/vaksinasi.php
@@ -0,0 +1,155 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <title>VAKSINASI</title>
+  </head>
+  <body>    
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">INFO COVID</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="/WebCovid">BERANDA</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="/">VAKSINASI</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+      <div class="container">
+        <br></br>
+        <h2> VAKSINASI COVID-19 </h2>
+        <h7> Situs ini merupakan Informasi seputar Covid-19 dari Tim 1 PKL SMKN2 Kota Sukabumi, berisi tentang
+update jumlah kasus dan vaksinasi serta informasi Covid-19. </h7>
+        <br></br>
+<h3>Situasi Vaksinasi Saat Ini</h3>
+      </div>
+
+    <style type="text/css">
+    .box{
+        padding: 15px 25px;
+        border-radius: 5px;
+    }
+    </style>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="bg-dark box text-danger">
+            <div class="row">
+            <div class="col-md-4">
+                <h3 id="data-kasus">40.349.049</h3>
+                <h5>Total Sasaran</h5>
+            </div>
+          </div>
+         </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="bg-dark box text-info">
+            <div class="row">
+            <div class="col-md-4">
+                <h3 id="data-mati">1.468.764</h3>
+                <h5>Tenaga Kesehatan</h5>
+            </div>
+          </div>
+        </div>
+        </div>
+
+          <div class="col-md-4">
+            <div class="bg-dark box text-success">
+            <div class="row">
+                 <div class="col-md-4">
+                <h3 id="data-sembuh">17.327.167</h3>
+                <h5>Petugas Publik</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4 mt-3">
+        <div class="bg-dark box text-primary">
+        <div class="row">
+             <div class="col-md-7">
+            <h3 id="data-sembuh">21.553.118</h3>
+            <h5>Lansia</h5>
+        </div>
+      </div>
+    </div>
+  </div>
+
+      <div class="container text-center">
+        <br></br>
+        <h3>COVID-19 </h3>
+      <div class="col-md-6">
+        <img src="gambar/cvectorhead.png">
+      </div>
+      </div>
+
+      <div class="container">
+        <h7> COVID-19 (coronavirus disease 2019) adalah jenis penyakit baru yang disebabkan oleh virus dari
+golongan coronavirus, yaitu SARS-CoV-2 yang juga sering disebut virus Corona.
+
+COVID-19 - Alodokter
+Kasus pertama penyakit ini terjadi di kota Wuhan, Cina, pada akhir Desember 2019. Setelah itu,
+COVID-19 menular antarmanusia dengan sangat cepat dan menyebar ke puluhan negara,
+termasuk Indonesia, hanya dalam beberapa bulan.
+
+Penyebarannya yang cepat membuat beberapa negara menerapkan kebijakan untuk memberlakukan
+lockdown untuk mencegah penyebaran virus Corona. Di Indonesia, pemerintah menerapkan kebijakan
+Pembatasan Sosial Berskala Besar (PSBB) untuk menekan penyebaran virus ini. </h7>
+        <br></br>
+      </div>
+      <div class="container text-center">
+        <br></br>
+        <h3>PENCEGAHAN </h3>
+      <div class="col-md-6">
+        <img src="gambar/poster.jpg">
+      </div>
+      </div>
+
+      <div class="container">
+        <h7> Untuk mencegah penyebaran COVID-19:
+- Cuci tangan Anda secara rutin. Gunakan sabun dan air, atau cairan pembersih tangan berbahan
+alkohol.
+- Selalu jaga jarak aman dengan orang yang batuk atau bersin.
+- Kenakan masker jika pembatasan fisik tidak dimungkinkan.
+- Jangan sentuh mata, hidung, atau mulut Anda.
+- Saat batuk atau bersin, tutup mulut dan hidung Anda dengan lengan atau tisu.
+- Jangan keluar rumah jika merasa tidak enak badan.
+- Jika demam, batuk, atau kesulitan bernapas, segera cari bantuan medis.
+- Telepon terlebih dahulu agar penyedia layanan kesehatan dapat segera mengarahkan Anda ke
+fasilitas kesehatan yang tepat. Tindakan ini akan melindungi Anda serta mencegah penyebaran
+virus dan infeksi lainnya.
+Masker
+Masker dapat membantu mencegah penyebaran virus dari orang yang mengenakannya kepada
+orang lain. Mengenakan masker saja tidak cukup untuk melindungi diri dari COVID-19, sehingga
+harus dikombinasikan dengan pembatasan fisik dan kebersihan tangan. Ikuti saran yang
+diberikan oleh otoritas kesehatan setempat. </h7>
+        <br></br>
+      </div>
+
+
+</div>
+</div>
+
+<footer class="bg-dark mt-3 bt-2 pb-2">
+<b class="text-white"> INFO COVID </b>
+<i class="text-white text-center"> PKL SMKN 2 Kota Sukabumi </i>
+</footer>
+       
+  </body>
+</html>
